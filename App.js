39ed801719b070c298d293e9402bbb717585b1ae
Sarthak <sarthak@beanbyte.dev>
2024-08-17 15:37:29 +0530

    ep_01_inception

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("div", {id: 'parent'}, [
+    React.createElement("div", {id: 'child1'}, [
+        React.createElement("h1", {}, "This is child1 h1 tag."),
+        React.createElement("h2", {}, "This is child1 h2 tag.")
+    ]),
+    React.createElement("div", {id: 'child2'}, [
+        React.createElement("h1", {}, "This is child2 h1 tag."),
+        React.createElement("h2", {}, "This is child2 h2 tag.")
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
